5831a6bfcc37afdfa18cd41aa64c7c62b6498e52
longsongkk <116349817+longsongkk@users.noreply.github.com>
2022-12-03 14:01:48 +0000

    Add files via upload

--- Task3/Signextension.sv
+++ Task3/Signextension.sv
@@ -1,16 +1,20 @@
-module Signextension(
-    input logic [11:0]        Imm_br,
-    input logic [11:0]       Imm_imm,
-    input logic              Immsrc,
+module Sign-extension(
+    input logic [31:7]        Imm,
+    //input logic [11:0]       Imm_imm,
+    input logic [1:0]        Immsrc,
     output logic [31:0]      ImmOp
 );
-assign [11:0] Imm;
+//assign [11:0] Imm;
 always_comb begin
-    if(Immsrc)
-        Imm<=Imm_mm;
-    else 
-        Imm<=Imm_br;
-
-    Immop={20{Imm[11],Imm[11:0]}};
+    case(Immsrc) 
+        2'b00:
+            Immop = {{20{Imm[31]}},Imm[31:20]};
+        2'b01:
+            Immop = {{20{Imm[31]}},Imm[31:25],Imm[11:7]},
+        2'b10:
+            Immop = {{20{Imm[31]}},Imm[7],Imm[30:25],Imm[11:8],1'b0};
+        2'b11:
+            Immop = {{12{Imm[31]}},Imm[19:12],Imm[20],Imm[30:21]};
+    endcase
 end
 endmoule
