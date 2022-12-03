801013ebcca9b79c2655da1677911d1e0c4051b4
longsongkk <116349817+longsongkk@users.noreply.github.com>
2022-12-03 17:27:57 +0000

    the new version of sinext and control

--- Task3/controlnew.sv
+++ Task3/controlnew.sv
@@ -3,19 +3,19 @@ module controlnew(
     input logic zero,
     output logic Pcsrc,
     output logic Resultsrc,
-    output logic MemWrite,
-    output logic [2:0] ALUctrl,
-    output logic ALUsrc,
+    output logic Memwrite,
+    output logic [2:0] Aluctrl,
+    output logic Alusrc,
     output logic [1:0] Immsrc,
-    output logic RegWrite
+    output logic Regwrite
 );
 
 logic [1:0] Aluop;
-always_comb begin
 
-    main a(zero, instr[6:0], Pcsrc, Resultsrc, MemWrite, ALUsrc,Immsrc,RegWrite,Aluop);
+
+    main a(zero, instr[6:0], Pcsrc, Resultsrc, Memwrite, Alusrc,Immsrc,RegWrite,Aluop);
 
     Alud b(Aluop,instr[5], instr[14:12],instr[15],ALUctrl);
-end
+
     
 endmodule
