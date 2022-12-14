ef9e89ac6ee7ff25a269093314a6d240a69a82ec
edzchen747 <edzchen747@gmail.com>
2022-12-14 13:14:20 +0000

    remove store opcodes as they do not go through sign-ext

--- cpu/sign_ext.sv
+++ cpu/sign_ext.sv
@@ -1,21 +1,19 @@
 module sign_ext (
     input logic [31:0]      Imm,
-    input logic [2:0]       ImmSrc,
+    input logic [1:0]       ImmSrc,
     output logic [31:0]     ImmOp
 );
 
 always_comb begin
     case(ImmSrc) 
-        3'b000:
+        3'b00:
             ImmOp = {{20{Imm[31]}},Imm[31:20]}; //lw lbu
-        3'b001:
-            ImmOp = {{20{Imm[31]}},Imm[31:25],Imm[11:7]}; //sw or sb
-        3'b010:
+        3'b01:
+            mmOp = {{Imm[31:12]}, 12'b0};   //LUI
+        3'b10:
             ImmOp = {{20{Imm[31]}},Imm[7],Imm[30:25],Imm[11:8],1'b0}; //beq bne
-        3'b011:
+        3'b11:
             ImmOp = {{12{Imm[31]}},Imm[19:12],Imm[20],Imm[30:21],1'b0}; //JAL
-        3'b111:
-            ImmOp = {{Imm[31:12]}, 12'b0};   //LUI
         default:
             ImmOp = {32'b0};
     endcase
