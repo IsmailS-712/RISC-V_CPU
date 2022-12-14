569799f60c8b2ead18ce1a6bf5ba40786470c860
m1h1r1 <116349028+m1h1r1@users.noreply.github.com>
2022-12-14 19:16:59 +0000

    Create f1lights_tb.cpp
    test bench for f1 lights

---
+++ f1lights_tb.cpp
@@ -0,0 +1,62 @@
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "Vtop.h"
+
+#include "vbuddy.cpp"     // include vbuddy code
+#define MAX_SIM_CYC 300
+
+int main(int argc, char **argv, char **env) {
+  int simcyc;     // simulation clock count
+  int tick;       // each clk cycle has two ticks for two edges
+
+  Verilated::commandArgs(argc, argv);
+  // init top verilog instance
+  Vtop* top = new Vtop;
+  // init trace dump
+  Verilated::traceEverOn(true);
+  VerilatedVcdC* tfp = new VerilatedVcdC;
+  top->trace (tfp, 99);
+  tfp->open ("top.vcd");
+ 
+  // init Vbuddy
+  if (vbdOpen()!=1) return(-1);
+  vbdHeader("PROJECT: RISC-V");
+
+  // initialize simulation inputs
+  top->clk = 1;
+  // top->TRIGGERSEL = 1;              include this line if using a physical trigger in the vbduddy
+  top->rst = 1;
+
+
+  // run simulation for MAX_SIM_CYC clock cycles
+  for (simcyc=0; simcyc<MAX_SIM_CYC; simcyc++) {
+    // dump variables into VCD file and toggle clock
+    for (tick=0; tick<2; tick++) {
+      tfp->dump (2*simcyc+tick);
+      top->clk = !top->clk;
+      top->eval ();
+    }
+    top->rst = 0;
+    // top->TRIGGERSEL = 1;             include this line if using a physical trigger in the vbduddy
+    
+    // plot ROM output and print cycle count
+    vbdHex(4, (int(top->a0) >> 16) & 0xF);
+    vbdHex(3, (int(top->a0) >> 8) & 0xF);
+    vbdHex(2, (int(top->a0) >> 4) & 0xF);
+    vbdHex(1, int(top->a0) & 0xF);
+    vbdCycle(simcyc+1);
+
+        // Display toggle neopixel
+   
+    vbdBar(top->a0);
+    top->rst = vbdFlag();
+    
+    // either simulation finished, or 'q' is pressed
+    if ((Verilated::gotFinish()) || (vbdGetkey()=='q')) 
+      exit(0);                // ... exit if finish OR 'q' pressed
+  }
+
+  vbdClose();     // ++++
+  tfp->close(); 
+  exit(0);
+}
